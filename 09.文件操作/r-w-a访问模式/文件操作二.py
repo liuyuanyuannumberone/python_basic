41ef6fe6f93fa339862de6ac69f4afdbc556889b
liuyuanyuan <1445179059@qq.com>
2020-08-07 09:46:51 +0800

    20200807目录整理

---
+++ 09.文件操作/r-w-a访问模式/文件操作二.py
@@ -0,0 +1,26 @@
+"""
+r  rb   r+  rb+ 
+w  wb   w+  wb+ 
+a  ab   a+  ab+ 
+
++  代表可读写
+b  表示二进制
+r/w/a  
+r 表示文件不存在报错，文件指针在开头      读
+w 表示文件存在则覆盖（open的时候就会直接覆盖），不存在则创建 ，文件指针在开头 写
+a 表示文件存在则追加，不存在则创建，文件指针在结尾      追加
+"""
+
+f1 = open('txt/readline.txt', 'r+')
+print(f1.read())
+f1.close()
+
+
+print("===")
+f2 = open('txt/readline.txt', 'a+')
+print(f2.read())
+f2.close()
+print("===")
+
+
+
---
+++ 09.面向对象/面向对象.py
@@ -0,0 +1,14 @@
+"""
+class 类名():
+    代码
+"""
+
+
+class Washer():
+    def wash(self):
+        print('能洗衣服')
+
+
+# 创建对象  对象名 =  类名()
+haier = Washer()
+haier.wash()
